915d1ef7706a55a3d9e4927e832f81acf0e71bac
mark-torres10 <markptorres1@gmail.com>
2022-02-26 17:12:50 -0500

    style: continue adding icons in the chat header

--- client/components/ChatHeader.jsx
+++ client/components/ChatHeader.jsx
@@ -4,18 +4,22 @@ import Image from 'next/image';
 require('dotenv').config()
 import styles from '../styles/chatHeader.module.css';
 import at from '../assets/icons/at.svg';
+import help from '../assets/icons/help.svg';
+import inbox from '../assets/icons/inbox.svg';
+import personPlus from '../assets/icons/person-plus.svg';
+import phone from '../assets/icons/phone.svg';
+import pin from '../assets/icons/pin.svg';
+import video from '../assets/icons/video.svg';
 import ethLogo from '../assets/eth.png'
 
 const testMetamaskAddress = process.env.METAMASK_ADDRESS;
 
 const ChatHeader = () => {
-
-    // const [currentAccount, setCurrentAccount] = useState(true);
-
-    const currentAccount = testMetamaskAddress;
+    const [currentAccount, setCurrentAccount] = useState(testMetamaskAddress);
 
     /* TODO(mark): implement connection to metamask */
     const connectWallet = () => {}
+
     return (
         <div className={styles.chatHeader}>
             <div className={styles.roomNameContainer}>
@@ -25,15 +29,68 @@ const ChatHeader = () => {
             </div>
             {currentAccount ? (
                 <div className={styles.connectedWallet}>
-                    <Image src={ethLogo} height={20} width={15} alt='ethLogo'/>
-                    <span className={styles.separator}>{'|'}</span>
-                    {currentAccount.slice(0,6)}...{currentAccount.slice(39)}
+                <Image src={ethLogo} height={20} width={15} alt='ethLogo' />
+                <span className={styles.separator}>{'|'}</span>
+                {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
                 </div>
             ) : (
                 <div className={styles.connectWallet} onClick={() => connectWallet()}>
-                    Connect Wallet
+                Connect Wallet
                 </div>
             )}
+            <div className={styles.headerIconsContainer}>
+                <div className={styles.headerItem}>
+                <Image
+                    height={25}
+                    width={25}
+                    src={phone}
+                    className={styles.svg}
+                    alt=''
+                />
+                </div>
+                <div className={styles.headerItem}>
+                <Image
+                    height={25}
+                    width={25}
+                    src={video}
+                    className={styles.svg}
+                    alt=''
+                />
+                </div>
+            </div>
+            <div className={styles.headerItem}>
+                <Image height={25} width={25} src={pin} className={styles.svg} alt='' />
+            </div>
+            <div className={styles.headerItem}>
+                <Image
+                    height={25}
+                    width={25}
+                    src={personPlus}
+                    className={styles.svg}
+                    alt=''
+                />
+            </div>
+            <div className={styles.headerItem}>
+                <input type='search' placeholder='Search' />
+            </div>
+            <div className={styles.headerItem}>
+                <Image
+                    height={25}
+                    width={25}
+                    src={inbox}
+                    className={styles.svg}
+                    alt=''
+                />
+            </div>
+            <div className={styles.headerItem}>
+                <Image
+                    height={25}
+                    width={25}
+                    src={help}
+                    className={styles.svg}
+                    alt=''
+                />
+            </div>
         </div>
     )
 }
