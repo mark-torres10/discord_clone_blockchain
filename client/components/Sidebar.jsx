f2287958b04fe548af0b84aef14cf45e4d96c2cd
mark-torres10 <markptorres1@gmail.com>
2022-02-26 14:15:36 -0500

    feat: first outline of app skeleton + sidebar component

---
+++ client/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/sidebar.module.css";
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
+const Sidebar = () => {
+  return (
+    <div className={styles.wrapper}>Sidebar</div>
+  )
+}
+
+export default Sidebar
