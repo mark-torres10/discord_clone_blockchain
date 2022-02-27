6f9180c2cc966bf5c9ace6a5641e649178ebead3
mark-torres10 <markptorres1@gmail.com>
2022-02-26 20:15:44 -0500

    feat: add states + hooks to context provider

--- client/context/context..js
+++ client/context/context..js
@@ -1,5 +1,5 @@
 import { createContext, useState, useEffect, useReducer } from 'react';
-// import { useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import Gun from 'gun';
 
 import 'dotenv/config';
@@ -23,6 +23,43 @@ const reducer = (state, action) => {
 }
 
 export const DiscordProvider = ({ children }) => {
+
+    // track various states
+    const router = useRouter();
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const [currentAccount, setCurrentAccount] = useState('');
+    const [roomName, setRoomName] = useState('');
+    const [placeholder, setPlaceholder] = useStatae('Message...');
+    const [messageText, setMessageText] = useState('');
+    const [currentUser, setCurrentUser] = useState();
+
+    useEffect(() => {
+        checkIfWalletIsConnected()
+    }, [])
+
+    const createUserAccount = async() => {
+        // if no account, create user account
+    }
+
+    // handle connections with metamask wallet
+    const checkIfWalletIsConnected = async() => {
+        if (!window.ethereum) return;
+        try {
+            const addressArray = await window.ethereum.request({
+                method: 'eth_accounts',
+            })
+            if (addressArray.length > 0) {
+                setCurrentAccount(addressArray[0]);
+                createUserAccount(addressArray[0]);
+            } else {
+
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
     return (
         <DiscordContext.Provider value={{}}>{ children }</DiscordContext.Provider>
     )
